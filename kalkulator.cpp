4006fa4f23e383f8448967408c83bd196814fa22
Liumena Feyruz <liumenafeyruz@gmail.com>
2025-10-08 14:27:36 +0700

    tambah fungsi fpb dan kombinasi

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// fungsi fpb
+int fpb(int a, int b) {
+    while (b != 0) {
+        int sisa = a % b;
+        a = b;
+        b = sisa;
+    }
+    return a;
+}
+
 //fungsi kpk
 int kpk(int a, int b) {
     return (a * b) / fpb (a, b);
@@ -15,8 +25,15 @@ long long factorial(int n) {
         return hasil;
 }
 
+// fungsi mencari kombinasi n! / (n - r)! * r!
+long long kombinasi(int n, int r) {
+    if (r > n) {
+        return 0;
+    }
+    return factorial(n) / (factorial(n - r) * factorial(r));
+}
+
 int main () {
-    qxcefcervtg
     return 0;
 }
 
