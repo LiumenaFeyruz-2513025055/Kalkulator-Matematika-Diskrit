2992b348fd78f7bb3c8f36b44dc7b61438368d6e
anisholikhatin23-ui <anisholikhatin23@gmail.com>
2025-10-08 14:44:02 +0700

    tambah fungsi utama

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -1,4 +1,14 @@
-//fungsi kpk
-int kpk(int a, int b) {
-    return (a * b) / fpb (a, b);
-    }
+// fungsi utama aplikasi kalkulator matematika diskret
+int main() {
+    int inputan;
+    int n, r;
+
+    while( inputan != 4) {
+        cout << "\nKALKULATOR MATEMATIKA DISKRIT" << endl;
+        cout << "1. kombinasi" << endl;
+        cout << "2. permutasi" << endl;
+        cout << "3. kpk dan fpb" << endl;
+        cout << "4. cek bilangan prima" << endl;
+
+        cout << "Pilih menu: ";
+        cin >> inputan;
