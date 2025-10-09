037ba7601df88a16e1efab00c551ee3a4a34fbb7
Liumena Feyruz <liumenafeyruz@gmail.com>
2025-10-09 08:22:54 +0700

    revisi kode

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -33,6 +33,14 @@ long long kombinasi(int n, int r) {
     return factorial(n) / (factorial(n - r) * factorial(r));
 }
 
+// fungsi mencari permutasi, n! / (n - r)!
+long long permutasi(int n, int r) {
+    if (r > n) {
+        return 0;
+    }
+    return factorial(n) / factorial(n - r);
+}
+
 //fungsi mengecek bilangan prima
 bool isPrima (int n) {
     if (n <= 1) return false;
@@ -43,7 +51,46 @@ bool isPrima (int n) {
     return true;
 }
 
-int main () {
-    return 0;
-}
+// fungsi utama aplikasi kalkulator matematika diskret
+int main() {
+    int inputan;
+    int n, r;
 
+    while( inputan != 5) {
+        cout << "\nKALKULATOR MATEMATIKA DISKRIT" << endl;
+        cout << "1. kombinasi" << endl;
+        cout << "2. permutasi" << endl;
+        cout << "3. kpk dan fpb" << endl;
+        cout << "4. cek bilangan prima" << endl;
+        cout << "5. keluar" << endl;
+
+        cout << "Pilih menu: ";
+        cin >> inputan;
+
+        switch (inputan)
+        {
+        // menu kombinasi
+        case 1:
+            cout << "Masukkan n: ";
+            cin >> n;
+            cout << "Masukkan nilai r: ";
+            cin >> r;
+            cout << "C(" << n << ", " << r << ") = " << kombinasi(n, r) << endl;
+            break;
+        // menu permutasi
+        case 2:
+            cout << "Masukkan nilai n: ";
+            cin >> n;
+            cout << "Masukkan nilai r: ";
+            cin >> r;
+            cout << "P(" << n << ", " << r << ") = " << permutasi(n, r) << endl;
+            break;
+        // menu fpb
+        case 3:
+            cout << "Masukkan angka pertama: ";
+            cin >> n;
+            cout << "Masukkan angka kedua: ";
+            cin >> r;
+            cout << "FPB dari " << n << " dan " << r << " adalah " << fpb(n, r) << endl;
+            cout << "KPK dari " << n << " dan " << r << " adalah " << kpk(n, r) << endl;
+            break;
